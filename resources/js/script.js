6f715512a3b5f90663bdff177b5574431943054f
Albert Carter <albert.r.carter@gmail.com>
2020-10-30 15:08:19 +0100

    add initial files

---
+++ resources/js/script.js
@@ -0,0 +1,278 @@
+if ($(window).width() < 1020) {}
+
+/* ===================================
+       Loader
+   ====================================== */
+$(window).on('load', function () {
+    $(".loader").fadeOut();
+    $(".pre-loader").delay(500).fadeOut("slow");
+});
+/* ===================================
+       Side Menu
+   ====================================== */
+if ($("#sidemenu_toggle").length) {
+    $("#sidemenu_toggle").on("click", function () {
+        $(".pushwrap").toggleClass("active");
+        $(".side-menu").addClass("side-menu-active"), $("#close_side_menu").fadeIn(700)
+    }), $("#close_side_menu").on("click", function () {
+        $(".side-menu").removeClass("side-menu-active"), $(this).fadeOut(200), $(".pushwrap").removeClass("active")
+    }), $(".side-nav .navbar-nav").on("click", function () {
+        $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+    }), $("#btn_sideNavClose").on("click", function () {
+        $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+    });
+}
+
+
+/* ===================================
+            Counter
+    ======================================*/
+(function ($) {
+    $.fn.countTo = function (options) {
+        options = options || {};
+
+        return $(this).each(function () {
+            // set options for current element
+            var settings = $.extend({}, $.fn.countTo.defaults, {
+                from: $(this).data('from'),
+                to: $(this).data('to'),
+                speed: $(this).data('speed'),
+                refreshInterval: $(this).data('refresh-interval'),
+                decimals: $(this).data('decimals')
+            }, options);
+
+            // how many times to update the value, and how much to increment the value on each update
+            var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                increment = (settings.to - settings.from) / loops;
+
+            // references & variables that will change with each update
+            var self = this,
+                $self = $(this),
+                loopCount = 0,
+                value = settings.from,
+                data = $self.data('countTo') || {};
+
+            $self.data('countTo', data);
+
+            // if an existing interval can be found, clear it first
+            if (data.interval) {
+                clearInterval(data.interval);
+            }
+            data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+            // initialize the element with the starting value
+            render(value);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+
+                render(value);
+
+                if (typeof (settings.onUpdate) == 'function') {
+                    settings.onUpdate.call(self, value);
+                }
+
+                if (loopCount >= loops) {
+                    // remove the interval
+                    $self.removeData('countTo');
+                    clearInterval(data.interval);
+                    value = settings.to;
+
+                    if (typeof (settings.onComplete) == 'function') {
+                        settings.onComplete.call(self, value);
+                    }
+                }
+            }
+
+            function render(value) {
+                var formattedValue = settings.formatter.call(self, value, settings);
+                $self.html(formattedValue);
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0, // the number the element should start at
+        to: 0, // the number the element should end at
+        speed: 1000, // how long it should take to count between the target numbers
+        refreshInterval: 100, // how often the element should be updated
+        decimals: 0, // the number of decimal places to show
+        formatter: formatter, // handler for formatting the value before rendering
+        onUpdate: null, // callback method for every time the element is updated
+        onComplete: null // callback method for when the element finishes updating
+    };
+
+    function formatter(value, settings) {
+        return value.toFixed(settings.decimals);
+    }
+}(jQuery));
+
+jQuery(function ($) {
+    // custom formatting example
+    $('.count-number').data('countToOptions', {
+        formatter: function (value, options) {
+            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+        }
+    });
+
+    // start all the timers
+    $('.timer').each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+});
+
+/* ===================================
+              Team Carousel
+    ======================================*/
+$('.team-section-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+/* ===================================
+            Cube Portfolio
+    ======================================*/
+
+$('#js-grid-mosaic-flat').cubeportfolio({
+    filters: '#js-filters-mosaic-flat',
+    layoutMode: 'mosaic',
+    sortByDimension: true,
+    mediaQueries: [{
+        width: 1500,
+        cols: 6,
+    }, {
+        width: 1100,
+        cols: 4,
+    }, {
+        width: 800,
+        cols: 3,
+    }, {
+        width: 480,
+        cols: 1,
+        options: {
+            gapHorizontal: 15,
+            gapVertical: 15,
+        }
+    }],
+    defaultFilter: '*',
+    animationType: 'fadeOutTop',
+    gapHorizontal: 0,
+    gapVertical: 0,
+    gridAdjustment: 'responsive',
+    caption: 'zoom',
+
+    // lightbox
+    lightboxDelegate: '.cbp-lightbox',
+    lightboxGallery: true,
+    lightboxTitleSrc: 'data-title',
+
+    plugins: {
+        loadMore: {
+            element: '#js-loadMore-mosaic-flat',
+            action: 'click',
+            loadItems: 3,
+        }
+    },
+});
+/* ===================================
+        WOW
+    ======================================*/
+
+new WOW().init();
+
+/* ===================================
+          REV SLIDER
+======================================*/
+if ($(".slider").length) {
+
+    $("#rev_slider_1_1").show().revolution({
+        sliderType: "standard",
+        jsFileLocation: "//localhost/reveditor/revslider/public/assets/js/",
+        sliderLayout: "fullscreen",
+        dottedOverlay: "none",
+        delay: 9000,
+        navigation: {
+            keyboardNavigation: "on",
+            keyboard_direction: "horizontal",
+            mouseScrollNavigation: "disable",
+            mouseScrollReverse: "default",
+            onHoverStop: "off",
+            arrows: {
+                style: "gyges",
+                enable: true,
+                hide_onmobile: true,
+                hide_under: 767,
+                hide_onleave: false,
+                tmp: '',
+                left: {
+                    h_align: "left",
+                    v_align: "center",
+                    h_offset: 10,
+                    v_offset: 0
+                },
+                right: {
+                    h_align: "right",
+                    v_align: "center",
+                    h_offset: 10,
+                    v_offset: 0
+                }
+            }
+        },
+
+        responsiveLevels: [1240, 1024, 778, 480],
+        visibilityLevels: [1240, 1024, 778, 480],
+        gridwidth: [1140, 1024, 778, 480],
+        gridheight: [700, 768, 960, 420],
+        lazyType: "none",
+        parallax: {
+            type: "mouse",
+            origo: "enterpoint",
+            speed: 400,
+            speedbg: 0,
+            speedls: 0,
+            levels: [2, 3, 5, 10, 25, 30, 35, 40, 45, 46, 47, 48, 49, 50, 51, 55],
+            disable_onmobile: "on"
+        },
+        shadow: 0,
+        spinner: "spinner2",
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+        shuffle: "off",
+        autoHeight: "off",
+        fullScreenAutoWidth: "off",
+        fullScreenAlignForce: "off",
+        fullScreenOffsetContainer: "",
+        fullScreenOffset: "",
+        disableProgressBar: "on",
+        hideThumbsOnMobile: "off",
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        debugMode: false,
+        fallbacks: {
+            simplifyAll: "off",
+            nextSlideOnWindowFocus: "off",
+            disableFocusListener: false,
+        }
+    });
+}
